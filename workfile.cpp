7e44f09dee04776431ba6c0925c8aeae27fb5f4c
BinaryKenny <khairullinvadim2006@gmail.com>
2025-11-16 09:54:00 +0300

    I deleted extras

--- workfile.cpp
+++ workfile.cpp
@@ -16,33 +16,7 @@ struct IntMatrix{
 
 int main()
 {
-  try
-  {
-    int next = 0;
-    std::cin >> next;
-    IntMatrix a(next);
-    while (std::cin >> next)
-    {
-      a.add(next);
-    }
-    if (std::cin.fail() and !std::cin.eof())
-    {
-      std::cerr << "Error: wrong input\n";
-      return 1;
-    }
-    size_t count = 1;
-    for (size_t i = 0; i < a.size() - 1; ++i)
-    {
-      int d = a.get(i);
-      count += !(d % a.last())? 1 : 0;
-    }
-    std::cout << count << "\n";
-  }
-  catch( const std::bad_alloc & error)
-  {
-    std::cerr << error.what() << "\n";
-    return 2;
-  }
+  
 }
 
 IntMatrix::~IntMatrix()
